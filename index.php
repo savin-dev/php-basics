82cf52e2d14284d07d4b6ffaeb2f743f4e48f721
savin-dev <niduwarafonseka@gmail.com>
2021-01-16 00:01:51 +0530

    variable

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First page</title>
+</head>
+<body>
 <?php
     echo "Hello world.";
+    $name = "Savin";
+    echo $name." Niduwara Fonseka.";
 ?>
+</body>
+</html>

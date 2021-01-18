83fc297f1a76436917cc19a9c1479b5126b97f3b
savin-dev <niduwarafonseka@gmail.com>
2021-01-18 14:39:49 +0530

    php calculator

--- index.php
+++ index.php
@@ -5,15 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>First page</title>
 </head>
+<form action="">
+    <label>
+        <input type="text" name="num1" placeholder="Number 1">
+        <input type="text" name="num2" placeholder="Number 2">
+    </label>
+    <label for="">
+        <select name="operator" id="">
+            <option value="None">None</option>
+            <option value="Add">Add</option>
+            <option value="Subtraction">Substraction</option>○○○
+            <option value="Multiplication">Multiplication</option>
+            <option value="Device">Devide</option>
+        </select>
+    </label>
+    <br/>
+    <button name="submit" type="submit" value="submit">Calculate</button><p>The answer is : </p>
+<br/>
 <body>
-<form method="GET">
-    <input type="text" name="person">
-    <button>Submit</button>
-</form>
 <?php
-    // This is a single line comment.
-    $name = $_GET['person'];
-    echo strlen($name." Niduwara Fonseka.");
+    if (isset($_GET['submit'])){
+        $result1 = $_GET['num1'];
+        $result2 = $_GET['num2'];
+        $operator = $_GET['operator'];
+        switch ($operator){
+            case "None":
+                echo "You have to select a operator.";
+                break;
+            case "Add":
+                echo $result1 + $result2;
+                break;
+            case "Subtraction":
+                echo $result1 - $result2;
+                break;
+            case "Multiplication":
+                echo $result1 * $result2;
+                break;
+            case "Device":
+                echo $result1 / $result2;
+                break;
+        }
+    }
 ?>
 </body>
 </html>
